57af3bd323c2093ae00b971dfad085977d451f00
Martin Blum <ozzmab@hotmail.com>
2020-10-27 01:06:51 -0300

    Modificando desafio clase 5

--- src/componentes/Item/item.js
+++ src/componentes/Item/item.js
@@ -2,17 +2,32 @@ import React, { useEffect, useState } from 'react';
 
 
 async function getItems(el) {
-    try {
-        const data = await new Promise( (res,rej)=>{
-            setTimeout(() => {
-                res (el)
-            }, 2000);
-        });
-        return data;
-    } 
-    catch (error) {
-        console.log(error);
-    }
+    // try {
+    //     const data = await new Promise( (res,rej)=>{
+    //         setTimeout(() => {
+    //             res (el)
+    //         }, 2000);
+    //     });
+    //     return data;
+    // } 
+    // catch (error) {
+    //     console.log(error);
+    // }
+
+    
+    const data = await new Promise( (res,rej)=>{
+        setTimeout(() => {
+            res (el)
+        }, 2000);
+    })
+    .then(res=>{
+        return res
+    })
+    .catch(e=>{
+        console.error(e)
+    })
+    return data;
+    
 }
 
 
