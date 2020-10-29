5476175665b25f7cec9932de4ad96ad1964b8757
Martin Blum <ozzmab@hotmail.com>
2020-10-28 21:28:07 -0300

    Agregando boton de BuyWidget

--- src/componentes/ItemDetail/itemDetail.js
+++ src/componentes/ItemDetail/itemDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Counter from '../Counter/counter';
 import './itemDetail.css';
+import BuyWidget from '../BuyWidget/buyWidget'
 
 export default function ItemDetail({ initial, stock, onAdd, itemDetail }) {
 
@@ -17,18 +18,30 @@ export default function ItemDetail({ initial, stock, onAdd, itemDetail }) {
         <>
             <div className="item-detail-container">
                 <div className="itemDetail-flex" style={{ width: 250 }} key={Item.id}>
-                    <div className="itemDetail-img"style={{ height:"auto" }}>
+                    <div className="itemDetail-img" style={{ height: "auto" }}>
                         <img src={Item.img} className="product-Img" alt={Item.description} />
                     </div>
                 </div>
                 <Counter
-                    style={{top:0}}
+                    style={{ top: 0 }}
                     stock={stock}
                     initial={initial}
                     onAdd={onAdd}
-                    />
-
+                />
+                <BuyWidget
+                    divClassName="buy-icon"
+                />
             </div>
         </>
     )
 }
+
+/* <BuyWidget
+divClassName="buy-icon list-items"
+/> */
+
+
+/* <div className="buy-item" style={{ position: "absolute" }}>
+<li><a className="list-items far fa-credit-card" href="#BuyButtom">Comprar</a></li>
+</div>
+</div> */
